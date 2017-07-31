8e51636dd625ff748ef7ea57b29c1a17b7b9c184
DanielUrsachi <udanny95@gmail.com>
2017-07-31 23:59:48 +0100

    Using FXML

--- 03.Sup/src/sample/Controller.java
+++ 03.Sup/src/sample/Controller.java
@@ -1,4 +1,15 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+import java.awt.*;
+
 public class Controller {
+    @FXML private TextField firstName;
+
+    public void handleSaySup(ActionEvent actionEvent) {
+        System.out.println("Sup " + firstName.getText() + " !!!");
+    }
 }
--- 03.Sup/src/sample/Main.java
+++ 03.Sup/src/sample/Main.java
@@ -23,33 +23,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        Group root = new Group(); // grup de elemente
-
-        Text txt = new Text("Sup?");
-        txt.setFont(new Font("Papyrus", 80));
-
-        TextField nameFld = new TextField();
-
-        Label label = new Label();
-        label.setText("Name: ");
-
-        Button btn = new Button();
-        btn.setText("Say Sup!");
-
-        GridPane gridPane = new GridPane();
-        gridPane.add(label, 0, 0);
-        gridPane.add(nameFld, 1, 0);
-        gridPane.add(btn, 1,1);
-        gridPane.setHgap(20); // gutter, locul liber dintre collumns
-        //gridPane.setGridLinesVisible(true); // arata liniile tabelului
-
-        btn.setOnAction(event -> System.out.println("Sup " + nameFld.getText() + " !!!")); //onAction:click prin lambda
-
-        VBox box = new VBox();
-        box.getChildren().addAll(txt, gridPane); // adaugarea in containter gen vertical layout
-
-        root.getChildren().add(box); // atribuirea layoutului in root
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
 
         primaryStage.setTitle("Sup");
         primaryStage.setScene(new Scene(root, 300, 275)); // adaugarea grupului de elemente in scena

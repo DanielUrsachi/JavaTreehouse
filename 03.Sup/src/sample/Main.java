98483403e3601910439347c1cfe3a90829cc9a02
DanielUrsachi <udanny95@gmail.com>
2017-08-01 01:28:38 +0100

    implementat css & restructurat resources

--- 03.Sup/src/sample/Main.java
+++ 03.Sup/src/sample/Main.java
@@ -2,28 +2,15 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import javafx.scene.control.Label;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-import javafx.scene.text.Font;
-
-
-
-import java.awt.*;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/sample.fxml"));
 
         primaryStage.setTitle("Sup");
         primaryStage.setScene(new Scene(root, 300, 275)); // adaugarea grupului de elemente in scena

b8c08c90d9da73145b6f93dddcb4c848dd7e6214
DanielUrsachi <udanny95@gmail.com>
2017-08-10 11:14:00 +0100

    adding .hbs (html), using post and cookies

--- 05.HelloSpark/src/main/java/Main.java
+++ 05.HelloSpark/src/main/java/Main.java
@@ -1,8 +1,30 @@
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import static spark.Spark.*;
 
 public class Main {
     public static void main(String[] args) {
         get("/hello", (req, res) -> "Hello World"); //get http method din URI-ul: /hello + prin lambda transmite Hello World
-        get("/", (req, res) -> "Hello Students");
+
+        get("/", (req, res) -> { //pentru afisarea html-ului din hbs
+            Map<String, String> model = new HashMap<>();
+
+            return new ModelAndView(null, "index.hbs"); // returneaza file-ul html
+
+        }, new HandlebarsTemplateEngine()); // metoda necesara pentu asta
+
+       post("/sign-in", (req, res) -> { // la metoda post
+           Map<String, String> model = new HashMap<>();
+           String username = req.queryParams("username");
+           res.cookie("username",username); //setam cookie dupa identificator si input
+           model.put("username", username); // astfel transmitem numele variabilei si valoarea sa
+            return new ModelAndView(model, "sign-in.hbs"); // arunca pe sign-in
+        }, new HandlebarsTemplateEngine());
+
+
     }
 }

6b7b6e1032dc77a746eed6335d11e928bc6c20cc
DanielUrsachi <udanny95@gmail.com>
2017-08-21 13:04:06 -0600

    conect model to view through controller

--- 07.GifLib/src/main/java/com/teamtreehouse/giflib/controller/GifController.java
+++ 07.GifLib/src/main/java/com/teamtreehouse/giflib/controller/GifController.java
@@ -1,16 +1,28 @@
 package com.teamtreehouse.giflib.controller;
 
+import com.teamtreehouse.giflib.model.Gif;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.xml.ws.RequestWrapper;
+import java.time.LocalDate;
 
 @Controller // indic asociatia cu controller de spring ( acest controller e pentru toate paginile )
 public class GifController {
     @RequestMapping(value = "/") // pentru link dintre root directory si metoda
-    ///@ResponseBody // raspunsul de la server, care exclude careva procesari suplimentare
-    public String listGifs(){ // metoda pentru afisarea tuturor GIF-urilor, ce handle URI-ul root
-        return "home"; /// tot aici putem include si un string, avind ResponsBody, afiseaza ca p, fara html template
+
+    public String listGifs() { // metoda pentru afisarea tuturor GIF-urilor, ce handle URI-ul root
+        return "home"; // html-ul inclus
+
+    }
+    @RequestMapping(value = "/gif")
+    public String gifDetails(ModelMap modelMap){ // link intre controller si model care il folosim
+        //compiler-bot.gif, el o vede din resources
+        Gif gif = new Gif("compiler-bot", LocalDate.of(2015,2,13), "Chris Ramacciotti", true);
+        modelMap.put("gif", gif); // adaug in model, obiectul necesar
+        return "gif-details";
+
     }
 }

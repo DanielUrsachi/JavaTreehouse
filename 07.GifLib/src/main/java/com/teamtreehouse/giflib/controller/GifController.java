c994ff0259d4a3a2f57d6615377fd4f62751c1bb
DanielUrsachi <udanny95@gmail.com>
2017-08-21 14:47:45 -0600

    exclude Hardcoding from DetailPage fo GIFs

--- 07.GifLib/src/main/java/com/teamtreehouse/giflib/controller/GifController.java
+++ 07.GifLib/src/main/java/com/teamtreehouse/giflib/controller/GifController.java
@@ -1,8 +1,11 @@
 package com.teamtreehouse.giflib.controller;
 
+import com.teamtreehouse.giflib.data.GifRepository;
 import com.teamtreehouse.giflib.model.Gif;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -11,18 +14,20 @@ import java.time.LocalDate;
 
 @Controller // indic asociatia cu controller de spring ( acest controller e pentru toate paginile )
 public class GifController {
-    @RequestMapping(value = "/") // pentru link dintre root directory si metoda
+    @Autowired // spune la framework to construct and assign gifrepository object as soon as it's needed
+    private GifRepository gifRepository; // in cazul in care nu-l initializam => nullPointerException, putem initializa in metoda sau prin AutoWeired
 
+    @RequestMapping(value = "/") // pentru link dintre root directory si metoda
     public String listGifs() { // metoda pentru afisarea tuturor GIF-urilor, ce handle URI-ul root
         return "home"; // html-ul inclus
 
     }
-    @RequestMapping(value = "/gif")
-    public String gifDetails(ModelMap modelMap){ // link intre controller si model care il folosim
+    @RequestMapping(value = "/gif/{name}") // prin {name}, noi preluam numele gif path
+    // prin @PathVariable , parsam name-ul preluat mai sus
+    public String gifDetails(@PathVariable String name, ModelMap modelMap){ // link intre controller si model care il folosim
         //compiler-bot.gif, el o vede din resources
-        Gif gif = new Gif("compiler-bot", LocalDate.of(2015,2,13), "Chris Ramacciotti", true);
+        Gif gif = gifRepository.findByName(name); // accesam lista de gif-uri, fara a initializa ca obiect, folosind autowired
         modelMap.put("gif", gif); // adaug in model, obiectul necesar
         return "gif-details";
-
     }
 }

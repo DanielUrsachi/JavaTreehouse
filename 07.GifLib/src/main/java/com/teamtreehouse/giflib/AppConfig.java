0b5eae2775107b16b266165af46c032efca493c8
DanielUrsachi <udanny95@gmail.com>
2017-08-21 01:31:26 -0600

    implements css and introduced some design stuff

--- 07.GifLib/src/main/java/com/teamtreehouse/giflib/AppConfig.java
+++ 07.GifLib/src/main/java/com/teamtreehouse/giflib/AppConfig.java
@@ -10,4 +10,6 @@ public class AppConfig {
     public static void main(String[] args) {
         SpringApplication.run(AppConfig.class, args); // pornesc web - aplicatia
     }
+    //TODO:Modeling, Storing, and Presenting Data course ! ! !
+    //TODO:Using the MVC Architecture course ! ! !
 }
